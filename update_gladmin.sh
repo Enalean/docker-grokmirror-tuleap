895a1a6f1e0ce0725a9422743642961a77a5b297
Manuel VACELET <manuel.vacelet@enalean.com>
2018-04-26 09:00:15 +0200

    Fix path

--- update_gladmin.sh
+++ update_gladmin.sh
@@ -7,6 +7,6 @@ if [ $gitname = gitolite-admin.git ]
 then
   cd $git
   export GL_BINDIR=/usr/share/gitolite3
-  export GL_LIBDIR=$GL_BINDIR
+  export GL_LIBDIR=$GL_BINDIR/lib
   $HOME/.gitolite/hooks/gitolite-admin/post-update refs/heads/master
 fi
